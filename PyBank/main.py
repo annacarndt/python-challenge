0918a2076774909bc1936ebcf118fae6ea199942
Anna Arndt <161068523+annacarndt@users.noreply.github.com>
2024-03-21 15:45:03 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -55,61 +55,51 @@ previous_profit_loss = None
 # open csv
 with open('/Users/annaarndt/Desktop/Bootcamp/python-challenge/PyBank/budget_data.csv', 'r') as file:
     next(file)
-    
-    # Iterate over each line in the file
     for line in file:
         # Split the line by comma
         data = line.strip().split(',')
-        
-        # Extract the date and profit/loss value
         date = data[0]
         profit_loss = int(data[1])
         
-        # If previous profit/loss exists, calculate the increase
+        # calculate the increase
         if previous_profit_loss is not None:
             increase = profit_loss - previous_profit_loss
-            
-            # Update the maximum increase if necessary
+        
             if increase > max_increase_amount:
                 max_increase_amount = increase
                 max_increase_date = date
-        
-        # Update the previous profit/loss value
+                
         previous_profit_loss = profit_loss
 
 print("Greatest increase in profits:")
 print("Date:", max_increase_date)
 print(max_increase_amount)
 
-# Initialize variables
+# definer variables
 max_decrease_date = None
 max_decrease_amount = float('inf')
 previous_profit_loss = None
 
-# Open the CSV file
+# open cvs 
 with open('/Users/annaarndt/Desktop/Bootcamp/python-challenge/PyBank/budget_data.csv', 'r') as file:
     # Skip the header
     next(file)
-    
-    # Iterate over each line in the file
+
     for line in file:
         # Split the line by comma
         data = line.strip().split(',')
-        
-        # Extract the date and profit/loss value
+
         date = data[0]
         profit_loss = int(data[1])
-        
-        # If previous profit/loss exists, calculate the decrease
+   
         if previous_profit_loss is not None:
             decrease = profit_loss - previous_profit_loss
             
-            # Update the maximum decrease if necessary
+            # maximum decrease
             if decrease < max_decrease_amount:
                 max_decrease_amount = decrease
                 max_decrease_date = date
-        
-        # Update the previous profit/loss value
+                
         previous_profit_loss = profit_loss
 
 print("Greatest decrease in profits:")
